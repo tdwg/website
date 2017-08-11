4100e5a2a502ef43d0261b35f946ceda8af73c9d
Peter Desmet <peter.desmet.work@gmail.com>
2017-08-11 19:58:28 +0200

    Rename settings files
    Default and local: settings.py
    Development stage: settings-dev.py
    Production stage: settings-prod.py

---
+++ settings-prod.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+# Use this file to generate the site in the *production* environment.
+# It will use (and overwrite some) settings from settings.py
+
+import os
+import sys
+sys.path.append(os.curdir)
+from settings import *
+
