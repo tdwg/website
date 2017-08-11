6f0b49e569ccbf433f4c7a1a2e4fafa586dc4c8b
Peter Desmet <peter.desmet.work@gmail.com>
2017-08-11 20:38:27 +0200

    Define default settings

--- settings.py
+++ settings.py
@@ -2,34 +2,60 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = "TDWG"
+# This file contains the default and local settings.
+# You can safely use these to generate the website locally with: pelican -s settings.py
+# Some of these settings will be overwritten in the development or production environment.
+
+# Site
 SITENAME = "TDWG"
 SITEURL = ""
-
-PATH = "content"
-
+AUTHOR = "TDWG"
 TIMEZONE = "Europe/Copenhagen"
-
 DEFAULT_LANG = "en"
 
-# Feed generation is usually not desired when developing
+# Display
+# THEME = ""
+DISPLAY_PAGES_ON_MENU = True
+DEFAULT_PAGINATION = False
+DEFAULT_DATE = 'fs'
+
+# URLs
+ARTICLE_URL = "news/{slug}.html"
+ARTICLE_SAVE_AS = "news/{slug}.html"
+
+# Content
+PATH = "content"
+ARTICLE_PATHS = ["news"]
+PAGE_PATHS = ["pages"]
+STATIC_PATHS = [
+	"images",
+	"files"
+]
+OUTPUT_PATH = "output" # Default
+DELETE_OUTPUT_DIRECTORY = True
+OUTPUT_RETENTION = ()
+CACHE_CONTENT = False
+
+# Feeds
+FEED_DOMAIN = SITEURL
+FEED_ATOM = None
+FEED_RSS = None
 FEED_ALL_ATOM = None
+FEED_ALL_RSS = None
 CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
+CATEGORY_FEED_RSS = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+TAG_FEED_ATOM = None
+TAG_FEED_RSS = None
+TRANSLATION_FEED_ATOM = None
+TRANSLATION_FEED_RSS = None
 
 # Blogroll
-LINKS = (("Pelican", "http://getpelican.com/"),
-         ("Python.org", "http://python.org/"),
-         ("Jinja2", "http://jinja.pocoo.org/"),
-         ("You can modify those links in your config file", "#"),)
+LINKS = ()
 
 # Social widget
-SOCIAL = (("You can add links in your config file", "#"),
-          ("Another social link", "#"),)
-
-DEFAULT_PAGINATION = False
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+SOCIAL = (
+	("Twitter", "https://twitter.com/tdwg"),
+    ("GitHub", "https://github.com/tdwg")
+)
