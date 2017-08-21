d4b3e63409ca2669bdb89e303a7c3083644d25de
Peter Desmet <peter.desmet.work@gmail.com>
2017-08-22 00:42:15 +0200

    Use tdwg theme

--- settings.py
+++ settings.py
@@ -114,13 +114,17 @@ GITHUB_URL = "https://github.com/tdwg/website"
 # pelican-bootstrap3, a theme with many options. Below are the settings to enable that theme.
 # Documentation: https://github.com/getpelican/pelican-themes/tree/master/pelican-bootstrap3
 
-THEME = "../pelican-themes/pelican-bootstrap3"
-DIRECT_TEMPLATES.append("search") # For tipue-search
-BOOTSTRAP_THEME = "flatly"
-GITHUB_USER = "tdwg"
-CC_LICENSE = "CC-BY"
+# THEME = "../pelican-themes/pelican-bootstrap3"
+# DIRECT_TEMPLATES.append("search") # For tipue-search
+# BOOTSTRAP_THEME = "flatly"
+# GITHUB_USER = "tdwg"
+# CC_LICENSE = "CC-BY"
 # pelican-bootstrap3 currently requires the i18n_subsites plugin, and the following JINJA_ENV:
 # (see https://github.com/getpelican/pelican-themes/issues/460#issuecomment-322174096)
-JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
-PLUGINS.append("i18n_subsites")
+# JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+# PLUGINS.append("i18n_subsites")
 
+# TDWG theme settings:
+THEME = "../tdwg-theme"
+SITEDESCRIPTION = ""
+DIRECT_TEMPLATES.append("search") # For tipue-search
