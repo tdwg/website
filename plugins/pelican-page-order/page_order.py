e9885f609b4c81e86b8c617dd0fa17daa720f674
Peter Desmet <peter.desmet.work@gmail.com>
2017-12-04 10:19:21 +0100

    Update page_order plugin to allow empty page_order in metadata
    See also https://github.com/akhayyat/pelican-page-order/pull/1

--- plugins/pelican-page-order/page_order.py
+++ plugins/pelican-page-order/page_order.py
@@ -9,12 +9,13 @@ from pelican import signals
 
 def set_page_order(generator):
     for page in generator.pages:
-        if hasattr(page, 'page_order'):
+        try:
             page.page_order = int(page.page_order)
-        elif 'DEFAULT_PAGE_ORDER' in generator.settings:
-            page.page_order = int(generator.settings['DEFAULT_PAGE_ORDER'])
-        else:
-            page.page_order = 100
+        except:
+            if 'DEFAULT_PAGE_ORDER' in generator.settings:
+                page.page_order = int(generator.settings['DEFAULT_PAGE_ORDER'])
+            else:
+                page.page_order = 100
 
 def register():
     signals.page_generator_finalized.connect(set_page_order)
